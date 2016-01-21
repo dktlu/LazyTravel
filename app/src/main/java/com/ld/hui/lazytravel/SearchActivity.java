12df8970baf2eaa02299b034f4a3b9277b8e6940
dktlh <724279138@qq.com>
2016-01-21 18:24:50 +0800

    dsd

--- app/src/main/java/com/ld/hui/lazytravel/SearchActivity.java
+++ app/src/main/java/com/ld/hui/lazytravel/SearchActivity.java
@@ -3,14 +3,16 @@ package com.ld.hui.lazytravel;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 /**
  * Created by tao on 2015/12/14.
  */
-public class SearchActivity extends Activity {
+public class SearchActivity extends Activity implements View.OnClickListener {
 
     private TextView tvLocation;
+    private TextView tvCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +25,11 @@ public class SearchActivity extends Activity {
 
     private void initViews() {
         tvLocation = (TextView) findViewById(R.id.tv_location);
+        tvCancel = (TextView) findViewById(R.id.tv_cancel);
     }
 
     private void bindViews() {
-
+        tvCancel.setOnClickListener(this);
     }
 
     private void initData() {
@@ -34,6 +37,17 @@ public class SearchActivity extends Activity {
         if (null != intent) {
             String city = getIntent().getStringExtra("city");
             tvLocation.setText(city);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_cancel:
+                SearchActivity.this.finish();
+                break;
+            default:
+                break;
         }
     }
 }
